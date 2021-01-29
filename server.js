7ca34833737a8acb8fedf292346d9d9005f844f8
Loucmane <lookmanbenali@gmail.com>
2021-01-29 19:16:10 +0100

    add {useNewUrlParser: true, useUnifiedTopology: true } to mongoose.connect to get rid of depreciation warning

--- server.js
+++ server.js
@@ -13,13 +13,16 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 //connect to mongo
-
+ 
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+.connect(db, {useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err)
+       console.error(err);
+    else
+       console.log("Connected to the mongodb"); 
+  });
 
-
+ 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on ${port}`));
