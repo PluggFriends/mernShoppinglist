2726219b02978b8f79050b3415f12690128f19cf
loucmane <lookmanebanli@gmail.com>
2021-02-05 12:09:55 +0100

    first bash commit? lets go?

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'react-strap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+(item) => item.id !== id;
